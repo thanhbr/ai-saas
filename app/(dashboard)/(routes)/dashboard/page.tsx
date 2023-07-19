f4cb73c645e347749439bc96503dfee9e0b64172
Thanh Br <thanh.mathe@upos.vn>
2023-07-19 21:56:16 +0700

    first commit

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
-export default function Home() {
+const DashboardPage = () => {
   return (
     <Button variant={'destructive'}>Click me</Button>
   )
 }
+
+export default DashboardPage
